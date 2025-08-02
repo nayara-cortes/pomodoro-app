e6b206e8ef9e9b2c4c62a3a7214938287151e94a
Nayara Cortés <nayaracortes.dev@gmail.com>
2025-08-02 15:24:47 +0200

    (17) Tachado + checkbox de una tarea completada

--- client/src/tasks.js
+++ client/src/tasks.js
@@ -13,24 +13,32 @@ function renderTasks() {
     task_ul.innerHTML = "";
 
     taskList.forEach((task, index, taskList) => {
-        // Creamos checkbox
-        const checkbox = document.createElement("span"); // Creamos un elemento span para poder darle comportamiento
-        checkbox.classList.add("task-checkbox"); // Le damos el estilo de la clase task-checkbox
-
-        // Si la tarea está completada, añadir tick al checkbox
-        if(task.completed === true) {
-            checkbox.classList.add("task-completed-checkbox");
-        }
-
-        // EVENT LISTENER
-        checkbox.addEventListener("click", () => completeTask(index));
 
         // Creamos li + checkbox
         let li = document.createElement("li"); // Creamos el elemento li
+
+        // Creamos checkbox
+        const checkbox = document.createElement("span"); // Creamos un elemento span para poder darle comportamiento
+        checkbox.classList.add("task-checkbox"); // Le damos el estilo de la clase task-checkbox
+        checkbox.addEventListener("click", () => completeTask(index));
+
+        // Creamos texto
+        const text = document.createElement("span"); // Creamos un texto nodo para poder añadirlo a li
+        text.classList.add("task-text");
+        text.textContent = task.text; 
+
+        // Si la tarea está completada, añadir tick al checkbox + cambiar estilo texto
+        if(task.completed === true) {
+            checkbox.classList.add("task-completed-checkbox");
+            text.classList.add("task-completed-text");
+        }
+
+        
+
+        
         li.appendChild(checkbox); // Le añadimos el checkbox
 
-        // Creamos texto y lo añadimos a li
-        const text = document.createTextNode(task.text); // Creamos un texto nodo para poder añadirlo a li
+        
         li.appendChild(text);
 
         // Añadimos el li a ul
@@ -39,8 +47,12 @@ function renderTasks() {
 }
 
 function completeTask(id) {
-    taskList[id].completed = true;
-    
+    if(taskList[id].completed === true) {
+        taskList[id].completed = false;
+    } else {
+        taskList[id].completed = true;
+    }
+    renderTasks();
 }
 
 renderTasks();
